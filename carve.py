07dc1d5bfa85d218eade7dfa46e53e554083050f
anatara7 <anatara7@cs.brown.edu>
2020-11-28 17:43:10 -0500

    fixed resolution issue

--- carve.py
+++ carve.py
@@ -18,6 +18,7 @@ def create_mask(img_filepath, parameter_file, angle_file, num):
 		 	extrinsic camera matrix (R|t) with extra row from identity matrix to make a 4x4 matrix
 	'''
 	img = Image.open(img_filepath).convert('1').filter(ImageFilter.BLUR).filter(ImageFilter.MinFilter(3)).filter(ImageFilter.MinFilter)
+	img = img.resize((int(img.size[0]), int(img.size[1])))
 	#plt.imshow(img)
 	#plt.show()
 	img = np.asarray(img, dtype='float32')
@@ -29,7 +30,6 @@ def create_mask(img_filepath, parameter_file, angle_file, num):
 	R = np.array([[float(parameter_str[9]), float(parameter_str[10]), float(parameter_str[11])], [float(parameter_str[12]), float(parameter_str[13]), float(parameter_str[14])], [float(parameter_str[15]), float(parameter_str[16]), float(parameter_str[17])]]);
 	t = np.array([[float(parameter_str[18])], [float(parameter_str[19])], [float(parameter_str[20])]]);
 	extrinsic = np.vstack((np.hstack((R, t)), np.array([[0.0, 0.0, 0.0, 1.0]], dtype='float32')))
-
 	camera_params = o3d.camera.PinholeCameraParameters()	
 	camera_params.extrinsic = extrinsic
 	camera_params.intrinsic.set_intrinsics(img.shape[1], img.shape[0], K[0,0], K[1,1], K[0,2], K[1,2])
@@ -43,16 +43,15 @@ def carve_sides(dir, param_file, angle_file):
 	Returns: carved voxelspace
 	'''
 	print("creating voxel space...")
-	voxel_grid = o3d.geometry.VoxelGrid.create_dense(origin=[320,320,320], color=[1,0,1], voxel_size=1, width =640, height =640, depth =640)
+	voxel_grid = o3d.geometry.VoxelGrid.create_dense(origin=[-1,-1,-1], color=[1,0,1], voxel_size=0.00325, width =2, height =2, depth=2)
 	print("created voxel space!")
 	
 	file_num = 1
 	for img_file in sorted(glob.glob(dir + '*.png')):
 		(img, params) = create_mask(img_file, param_file, angle_file, file_num)
 		voxel_grid.carve_silhouette(img, params)
+		print(file_num)
 		file_num = file_num + 1
 		
-		print(file_num)
-		o3d.visualization.draw_geometries([voxel_grid])
-
+	o3d.visualization.draw_geometries([voxel_grid])
 carve_sides('./temple/','./temple/temple_par.txt', './temple/temple_ang.txt')
